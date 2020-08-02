49dacdd3648f540d8ccb5a603aba6fac54a33824
amfosh <afasciocco@gmail.com>
2020-08-02 18:37:23 -0400

    kelvin conversion

--- resources/src/index.js
+++ resources/src/index.js
@@ -5,17 +5,20 @@ const forecast = document.getElementById('forecast');
 function fetchWeather(e) {
     e.preventDefault();
     const zip = document.querySelector("#search-term").value;
-    fetch(`https://pro.openweathermap.org/data/2.5/forecast/hourly?zip=${zip}&appid=b8d555814d3e396548e9371b2e5b9209`)
+    fetch(`https://api.openweathermap.org/data/2.5/forecast?zip=${zip}&appid=6e4234240a11d3e8ecc1803c3f8845e4`)
       .then(response => response.json())
       .then(data => {
         console.log(data)
     
           
-        // let output = `Forecast for ${data[0].EnglishName}, ${data[0].AdministrativeArea.ID} ${zip}`
-        
+        let output = `Forecast for ${data.city.name}, ${zip}`
+        let kelvin = Number(data.list[0].main.feels_like)
+        const celsius = kelvin - 273;
+        console.log(kelvin)
+        let temp = Math.floor(celsius * (9/5) + 32);
 
-        // // output += '</p>'
-        // output += `${data[0].fl}`
+        output += '</p>'
+        output += `Feels Like: ${temp}`
         // output += '</p>'
         // output += '<h3>definitions:</h3>'
         // output += '<p>'
